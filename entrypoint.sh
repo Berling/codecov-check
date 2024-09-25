bf1a5017f8df1e83097a109f314aa3c792697b1b
Georg Schäfer <georg.peter.schaefer@gmail.com>
2024-09-25 20:21:02 +0200

    fix: make result calculation failable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 set -ex
 REPO_NAME=$(echo "$GITHUB_REPOSITORY" | sed -e "s/^$GITHUB_REPOSITORY_OWNER\///") || exit 1
 TOTAL_COVERAGE=$(curl --location --request GET "https://codecov.io/api/v2/github/$GITHUB_REPOSITORY_OWNER/repos/$REPO_NAME/totals?branch=$GITHUB_REF_NAME" --header "Authorization: bearer $INPUT_TOKEN" | jq .totals.coverage) || exit 1
-if [ "$(echo "$TOTAL_COVERAGE < $INPUT_THRESHOLD" | bc)" -eq 1  ]; then
+RESULT=$(echo "$TOTAL_COVERAGE < $INPUT_THRESHOLD" | bc) || exit 1
+if [ "$RESULT" -eq 1  ]; then
   echo "::error title=Code Coverage Target Missed::Total code coverage is below threshold of $INPUT_THRESHOLD%"
   exit 1
 fi
