46d56bd5f26bacce7b073922f4ad76cb3f98b785
Georg Schäfer <georg.peter.schaefer@gmail.com>
2024-09-25 20:16:55 +0200

    fix: install missing applications to docker image

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM ubuntu:latest
 
+RUN apt update && apt install -y curl jq bc
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -ex
-export REPO_NAME=$(echo "$GITHUB_REPOSITORY" | sed -e "s/^$GITHUB_REPOSITORY_OWNER\///")
-export TOTAL_COVERAGE=$(curl --location --request GET "https://codecov.io/api/v2/github/$GITHUB_REPOSITORY_OWNER/repos/$REPO_NAME/totals?branch=$GITHUB_REF_NAME" --header "Authorization: bearer $INPUT_TOKEN" | jq .totals.coverage)
+REPO_NAME=$(echo "$GITHUB_REPOSITORY" | sed -e "s/^$GITHUB_REPOSITORY_OWNER\///") || exit 1
+TOTAL_COVERAGE=$(curl --location --request GET "https://codecov.io/api/v2/github/$GITHUB_REPOSITORY_OWNER/repos/$REPO_NAME/totals?branch=$GITHUB_REF_NAME" --header "Authorization: bearer $INPUT_TOKEN" | jq .totals.coverage) || exit 1
 if [ "$(echo "$TOTAL_COVERAGE < $INPUT_THRESHOLD" | bc)" -eq 1  ]; then
   echo "::error title=Code Coverage Target Missed::Total code coverage is below threshold of $INPUT_THRESHOLD%"
   exit 1
