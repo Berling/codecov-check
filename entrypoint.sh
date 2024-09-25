ed44ac6fde0ebdb22a952332ed75a9299c07d9c9
Georg Schäfer <georg.peter.schaefer@gmail.com>
2024-09-25 20:07:54 +0200

    fix: replace github action variable syntax in shell script with shell syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -ex
-export REPO_NAME=$(echo "${{ github.repository }}" | sed -e "s/^${{ github.repository_owner }}\///")
-export TOTAL_COVERAGE=$(curl --location --request GET "https://codecov.io/api/v2/github/${{ github.repository_owner }}/repos/$REPO_NAME/totals?branch=${{ github.ref_name }}" --header "Authorization: bearer ${{ inputs.token }}" | jq .totals.coverage)
-if [ "$(echo "$TOTAL_COVERAGE < ${{ inputs.threshold }}" | bc)" -eq 1  ]; then
-  echo "::error title=Code Coverage Target Missed::Total code coverage is below threshold of ${{ inputs.threshold }}%"
+export REPO_NAME=$(echo "$GITHUB_REPOSITORY" | sed -e "s/^$GITHUB_REPOSITORY_OWNER\///")
+export TOTAL_COVERAGE=$(curl --location --request GET "https://codecov.io/api/v2/github/$GITHUB_REPOSITORY_OWNER/repos/$REPO_NAME/totals?branch=$GITHUB_REF_NAME" --header "Authorization: bearer $INPUT_TOKEN" | jq .totals.coverage)
+if [ "$(echo "$TOTAL_COVERAGE < $INPUT_THRESHOLD" | bc)" -eq 1  ]; then
+  echo "::error title=Code Coverage Target Missed::Total code coverage is below threshold of $INPUT_THRESHOLD%"
   exit 1
 fi
